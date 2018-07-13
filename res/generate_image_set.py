5278222c6022c7eb3b0c96973288532b0d6601e3
LengyelR <richard.lengyel@yahoo.com>
2018-07-13 01:18:39 +0100

    faster generation (6x)
     - replaced bw_round with cuda method
     - simple lookups instead of casts

--- res/generate_image_set.py
+++ res/generate_image_set.py
@@ -1,56 +1,55 @@
 import numpy as np
 from PIL import Image
-import time
 import tqdm
+from numba import cuda, int16
+
 
 IMG_PATH = r'pic_128.jpg'
 OUTPUT_PATH = r'prime_candidates.txt'
 # ubiquity of primes: ln(10) ~ 2.3
 # only generating odd numbers, this is more than enough
-LIMIT = int(128 * 128 * 2.3)  
+LIMIT = int(128 * 128 * 2.3)
 
 
-def odd_round(s):
-    n = int(s[-1])
-    if n % 2 == 0:
-        n = n+1 if np.random.rand() > 0.5 else n-1
-    return s[:-1] + str(n)
+@cuda.jit(argtypes=[int16[:, :]], target='gpu')
+def _bw(image):
+    x, y = cuda.grid(2)
+    image[x, y] = int(image[x, y] / 25.5) if image[x, y] < 255 else 9
 
 
-def bw_round(px):
-    d = int((px / 255.0) * 10)
-    if d > 9:
-        return 9
-    if d < 0:
-        return 0
-    return d
+def bw_cuda(mtx):
+    return _bw[(4, 4), (32, 32)](mtx)
 
 
-img = Image.open(IMG_PATH)
-seen_before = set()
-quantize = np.vectorize(bw_round, otypes=[np.uint8])
+def get_candidates():
+    img = Image.open(IMG_PATH)
+    seen_before = set()
+    to_string = [str(i) for i in range(10)]
+    to_int = {str(i): i for i in range(10)}
 
-started = time.time()
-for _ in tqdm.trange(LIMIT):
-    while True:
-        bw_img = img.convert('L')
-        bw_mtx = np.array(bw_img, dtype='int16')
-        bw_mtx += np.random.randint(-2, 3, size=(128, 128), dtype='int16')
+    for _ in tqdm.trange(LIMIT):
+        while True:
+            bw_img = img.convert('L')
+            bw_mtx = np.array(bw_img, dtype='int16')
+            bw_mtx += np.random.randint(0, 3, size=(128, 128), dtype='int16')
 
-        bw_mtx = quantize(bw_mtx)
-        digits = bw_mtx.flatten()
+            bw_cuda(bw_mtx)
+            digits = bw_mtx.flatten()
+            num = "".join(to_string[d] for d in digits)
 
-        num = odd_round("".join(str(d) for d in digits))
-        num = num if len(num) == 128*128 else num[1:]
+            n = to_int[num[-1]]
+            if n % 2 == 0:
+                n = n + 1 if np.random.rand() > 0.5 else n - 1
+            num = num[:-1] + to_string[n]
 
-        if len(num) == 128*128 and num not in seen_before:
-            seen_before.add(num)
-            break
+            if len(num) == 128*128 and num not in seen_before:
+                seen_before.add(num)
+                break
 
-finished = time.time()
-print('generation took', (finished - started) / 60, 'minutes')
+    return seen_before
 
-with open(OUTPUT_PATH, 'w') as f:
-    f.writelines((str(num) + '\n' for num in seen_before))
-write_end = time.time()
-print('writing took:', (write_end - finished) / 60, 'minutes')
+
+if __name__ == "__main__":
+    candidates = get_candidates()
+    with open(OUTPUT_PATH, 'w') as f:
+        f.writelines((str(num) + '\n' for num in candidates))
